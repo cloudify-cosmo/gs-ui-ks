9f8f9054f47567c3b4c8717200318c1b84d2d5b8
eliranmal <eliranmal@gmail.com>
2013-10-23 17:28:59 +0300

    GS-11422 edges rendering

--- app/scripts/controllers/topology.js
+++ app/scripts/controllers/topology.js
@@ -4,158 +4,221 @@ angular.module('gsUiKsApp')
     .controller('TopologyCtrl', ['$scope', '$filter', 'Layout', 'Render', 'gs.config', function ($scope, $filter, Layout, Render, gsConfig) {
 
         $scope.data = {
-            graph: {
-                /*
-                'nodes': [{
-            'id': 1,
-            'name': 'vagrant_host',
-            'type': ['cloudify.tosca.types.host']
-        }, {
-            'id': 2,
-            'name': 'pickle_db',
-            'type': ['cloudify.tosca.types.db_server', 'cloudify.tosca.types.middleware_server']
-        }, {
-            'id': 3,
-            'name': 'flask',
-            'type': ['cloudify.tosca.types.web_server', 'cloudify.tosca.types.middleware_server']
-        }, {
-            'id': 4,
-            'name': 'flask_app',
-            'type': ['cloudify.tosca.types.app_module']
-        }],
-            'edges': [{
-            'type': 'contained_in',
-            'source': 2,
-            'target': 1
-        }, {
-            'type': 'contained_in',
-            'source': 3,
-            'target': 1
-        }, {
-            'type': 'contained_in',
-            'source': 4,
-            'target': 3
-        }, {
-            'type': 'connected_to',
-            'source': 4,
-            'target': 2
-        }]
-                */
+            graph:
+
+            {
                 'nodes': [
                     {
                         'id': 9,
                         'name': 'Network',
-                        'type': ['cloudify.tosca.types.network']
+                        'type': ['cloudify.types.network']
                     },
                     {
                         'id': 10,
                         'name': 'Tier',
-                        'type': ['cloudify.tosca.types.tier']
+                        'type': ['cloudify.types.tier']
                     },
                     {
                         'id': 11,
                         'name': 'Host A',
-                        'type': ['cloudify.tosca.types.host']
+                        'type': ['cloudify.types.host']
                     },
                     {
                         'id': 12,
                         'name': 'Host B',
-                        'type': ['cloudify.tosca.types.host']
+                        'type': ['cloudify.types.host']
                     },
                     {
                         'id': 13,
                         'name': 'Host C',
-                        'type': ['cloudify.tosca.types.host']
+                        'type': ['cloudify.types.host']
                     },
                     {
                         'id': 14,
                         'name': 'Pickle',
-                        'type': ['cloudify.tosca.types.db_server', 'cloudify.tosca.types.middleware_server']
+                        'type': ['cloudify.types.db_server', 'cloudify.types.middleware_server']
                     },
                     {
                         'id': 15,
                         'name': 'Flask',
-                        'type': ['cloudify.tosca.types.web_server', 'cloudify.tosca.types.middleware_server']
+                        'type': ['cloudify.types.web_server', 'cloudify.types.middleware_server']
                     },
                     {
                         'id': 16,
                         'name': 'App',
-                        'type': ['cloudify.tosca.types.app_module']
+                        'type': ['cloudify.types.app_module']
                     }
                 ],
                 'edges': [
                     {
-                        'type': 'contained_in',
+                        'type': 'cloudify.relationships.contained_in',
                         'source': 10,
                         'target': 9
                     },
                     {
-                        'type': 'contained_in',
+                        'type': 'cloudify.relationships.contained_in',
                         'source': 11,
                         'target': 10
                     },
                     {
-                        'type': 'contained_in',
+                        'type': 'cloudify.relationships.contained_in',
                         'source': 12,
                         'target': 10
                     },
                     {
-                        'type': 'contained_in',
+                        'type': 'cloudify.relationships.contained_in',
                         'source': 13,
                         'target': 10
                     },
                     {
-                        'type': 'contained_in',
+                        'type': 'cloudify.relationships.contained_in',
                         'source': 14,
                         'target': 12
                     },
                     {
-                        'type': 'contained_in',
+                        'type': 'cloudify.relationships.contained_in',
                         'source': 15,
                         'target': 12
                     },
                     {
-                        'type': 'contained_in',
+                        'type': 'cloudify.relationships.contained_in',
                         'source': 16,
                         'target': 15
                     },
-/*
                     {
-                        'type': 'connected_to',
-                        'source': 12,
-                        'target': 11
-                    },
-*/
-                    {
-                        'type': 'connected_to',
-                        'source': 11,
+                        'type': 'cloudify.relationships.connected_to',
+                        'source': 13,
                         'target': 9
                     }
                 ]
-            },
+            }
+
+/*            {
+                "nodes": [
+                    {
+                        "id": 1,
+                        "name": "webserver_host",
+                        "type": [
+                            "mezzanine_host",
+                            "cloudify.types.host"
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "name": "postgres_host",
+                        "type": [
+                            "mezzanine_host",
+                            "cloudify.types.host"
+                        ]
+                    },
+                    {
+                        "id": 3,
+                        "name": "nginx",
+                        "type": [
+                            "mezzanine_middleware",
+                            "cloudify.types.middleware_server"
+                        ]
+                    },
+                    {
+                        "id": 4,
+                        "name": "unicorn",
+                        "type": [
+                            "mezzanine_middleware",
+                            "cloudify.types.middleware_server"
+                        ]
+                    },
+                    {
+                        "id": 5,
+                        "name": "postgres_server",
+                        "type": [
+                            "mezzanine_middleware",
+                            "cloudify.types.middleware_server"
+                        ]
+                    },
+                    {
+                        "id": 6,
+                        "name": "postgres_server_db",
+                        "type": [
+                            "mezzanine_app_module",
+                            "cloudify.types.app_module"
+                        ]
+                    },
+                    {
+                        "id": 7,
+                        "name": "mezanine_app",
+                        "type": [
+                            "mezzanine_app_module",
+                            "cloudify.types.app_module"
+                        ]
+                    }
+                ],
+                "edges": [
+                    {
+                        "source": 3,
+                        "target": 4,
+                        "type": "cloudify.relationships.connected_to"
+                    },
+                    {
+                        "source": 3,
+                        "target": 1,
+                        "type": "cloudify.relationships.contained_in"
+                    },
+                    {
+                        "source": 4,
+                        "target": 1,
+                        "type": "cloudify.relationships.contained_in"
+                    },
+                    {
+                        "source": 5,
+                        "target": 2,
+                        "type": "cloudify.relationships.contained_in"
+                    },
+                    {
+                        "source": 6,
+                        "target": 5,
+                        "type": "cloudify.relationships.contained_in"
+                    },
+                    {
+                        "source": 7,
+                        "target": 4,
+                        "type": "cloudify.relationships.contained_in"
+                    },
+                    {
+                        "source": 7,
+                        "target": 3,
+                        "type": "cloudify.relationships.depends_on"
+                    },
+                    {
+                        "source": 7,
+                        "target": 6,
+                        "type": "cloudify.relationships.connected_to"
+                    }
+                ]
+            }*/,
+
             tree: {
                 'id': 'root',
                 'children': [
                     {
                         'id': 0,
-                        'type': ['cloudify.tosca.types.network'],
+                        'type': ['cloudify.types.network'],
                         'dependencies': [3],
                         'children': [
                             {
                                 'id': 2,
-                                'type': ['cloudify.tosca.types.tier'],
+                                'type': ['cloudify.types.tier'],
                                 'children': [
                                     {
                                         'id': 3,
-                                        'type': ['cloudify.tosca.types.host'],
+                                        'type': ['cloudify.types.host'],
                                         'children': [
                                             {
                                                 'id': 4,
-                                                'type': ['cloudify.tosca.types.web_server', 'cloudify.tosca.types.middleware_server'],
+                                                'type': ['cloudify.types.web_server', 'cloudify.types.middleware_server'],
                                                 'children': [
                                                     {
                                                         'id': 5,
-                                                        'type': ['cloudify.tosca.types.app_module']
+                                                        'type': ['cloudify.types.app_module']
                                                     }
                                                 ]
                                             }
@@ -165,7 +228,7 @@ angular.module('gsUiKsApp')
                             },
                             {
                                 'id': 1,
-                                'type': ['cloudify.tosca.types.tier'],
+                                'type': ['cloudify.types.tier'],
                                 'dependencies': [4, 5]
                             }
                         ]
