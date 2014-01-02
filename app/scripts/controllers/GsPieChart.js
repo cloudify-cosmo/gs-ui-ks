c917fb44254d38f1a13f6c50e71f24fea7c33b27
guym <guym@gigaspaces.com>
2014-01-02 09:43:00 +0200

    adding pie chart

---
+++ app/scripts/controllers/GsPieChart.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('gsUiKsApp')
+  .controller('GsPieChartCtrl', function ($scope) {
+        var randomData = function(size){
+            return d3.range(size).map(function(d){
+                return Math.ceil(Math.random() * 50);
+            })
+        };
+        $scope.count = 5;
+        $scope.chartData = randomData($scope.count);
+        $scope.changeData = function(){
+            $scope.chartData = randomData($scope.count);
+        }
+  });
