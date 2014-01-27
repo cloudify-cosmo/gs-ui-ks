5f4f84997fd69476b262ed773aca9da831fd160a
Itsik <extropia@gmail.com>
2014-01-27 11:12:48 +0200

    Blueprint Arrows & drag n drop

---
+++ test/spec/controllers/blueprintArrows.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: BlueprintarrowsCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('gsUiKsApp'));
+
+  var BlueprintarrowsCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    BlueprintarrowsCtrl = $controller('BlueprintarrowsCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
