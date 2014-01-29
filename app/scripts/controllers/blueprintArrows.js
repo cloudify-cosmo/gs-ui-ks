39f3acce2adc9f5b7e5cfa0336b6972303d97e8f
eliranmal <eliranmal@gmail.com>
2014-01-29 11:06:47 +0200

    fixing jshint errors.

--- app/scripts/controllers/blueprintArrows.js
+++ app/scripts/controllers/blueprintArrows.js
@@ -9,8 +9,8 @@ angular.module('gsUiKsApp')
             $scope.circles = [];
             for (var i = 0; i < $scope.count; i++) {
                 $scope.circles.push({
-                    top: (Math.random() * 450) + "px",
-                    left: (Math.random() * 450) + "px"
+                    top: (Math.random() * 450) + 'px',
+                    left: (Math.random() * 450) + 'px'
                 });
             }
             blueprintCoordinateService.draw();
@@ -19,15 +19,15 @@ angular.module('gsUiKsApp')
         $scope.Shuffle = function () {
             $scope.circles.forEach(function (o, i) {
                 $scope.circles[i] = {
-                    top: (Math.random() * 450) + "px",
-                    left: (Math.random() * 450) + "px"
+                    top: (Math.random() * 450) + 'px',
+                    left: (Math.random() * 450) + 'px'
                 };
             });
             blueprintCoordinateService.draw();
-        }
+        };
 
         function randomMap() {
-            $scope.map = []
+            $scope.map = [];
             for (var i = 0; i < $scope.count; i++) {
                 $scope.map.push(randomNumbers());
             }
@@ -38,28 +38,30 @@ angular.module('gsUiKsApp')
             var source = Math.round(Math.random() * $scope.count),
                 target = false;
 
-            setTarget();
             function setTarget() {
                 target = Math.round(Math.random() * $scope.count);
-                if(source == target)
+                if (source === target) {
                     setTarget();
+                }
             }
+
+            setTarget();
+
             return {
-                "source": source,
-                "target": target
-            }
+                source: source,
+                target: target
+            };
         }
 
         $scope.coordinates = blueprintCoordinateService.getCoordinates();
 
-        $scope.$watch("count", function () {
+        $scope.$watch('count', function () {
             randomCircles();
             randomMap();
         });
 
 
         $scope.tabs = [
-            // TODO is there a better way to get source code (perhaps $http or $resource)?
             {
                 title: $scope.tabsMeta.view.title,
                 content: '&lt;ANY blueprint:arrows=&quot;{array}&quot; color=&quot;{string}&quot; arrow=&quot;{bool}&quot; storke=&quot;{number}&quot;&gt;... &lt;/ANY&gt;'
@@ -67,19 +69,19 @@ angular.module('gsUiKsApp')
         ];
 
         var jsTabDisplay = false;
-        $scope.$watch("coordinates", function(coordinates){
-            if(coordinates.length > 0 && !jsTabDisplay) {
+        $scope.$watch('coordinates', function (coordinates) {
+            if (coordinates.length > 0 && !jsTabDisplay) {
                 $scope.tabs.push({
                     title: $scope.tabsMeta.controller.title,
                     content: '$scope.coordinates = ' + JSON.stringify(coordinates, $scope.themeReplacer, 2)
                 });
                 jsTabDisplay = true;
             }
-        }, true)
+        }, true);
 
 
-        $scope.dropend = function( item ) {
+        $scope.dropend = function (/*item*/) {
             blueprintCoordinateService.draw();
-        }
+        };
 
     });
