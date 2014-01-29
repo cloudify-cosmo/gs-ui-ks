5acc7d24926a379c566d7b873963290a65d2d180
Itsik <extropia@gmail.com>
2014-01-29 12:47:39 +0200

    PieProgress

---
+++ test/spec/controllers/pieProgress.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: PieprogressCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('gsUiKsApp'));
+
+  var PieprogressCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    PieprogressCtrl = $controller('PieprogressCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
