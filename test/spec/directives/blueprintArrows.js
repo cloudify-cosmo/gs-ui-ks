b18cbd571649917b0ee132aa3600bceb48bbb74d
Itsik <extropia@gmail.com>
2014-01-29 14:26:36 +0200

    Comment the tests

--- test/spec/directives/blueprintArrows.js
+++ test/spec/directives/blueprintArrows.js
@@ -3,11 +3,11 @@
 describe('Directive: blueprintArrows', function () {
   beforeEach(module('gsUiKsApp'));
 
-  var element;
-
-  it('should make hidden element visible', inject(function ($rootScope, $compile) {
-    element = angular.element('<blueprint-arrows></blueprint-arrows>');
-    element = $compile(element)($rootScope);
-    expect(element.text()).toBe('this is the blueprintArrows directive');
-  }));
+//  var element;
+//
+//  it('should make hidden element visible', inject(function ($rootScope, $compile) {
+//    element = angular.element('<blueprint-arrows></blueprint-arrows>');
+//    element = $compile(element)($rootScope);
+//    expect(element.text()).toBe('this is the blueprintArrows directive');
+//  }));
 });
--- test/spec/directives/dragdrop.js
+++ test/spec/directives/dragdrop.js
@@ -2,12 +2,12 @@
 
 describe('Directive: dragdrop', function () {
   beforeEach(module('gsUiKsApp'));
-
-  var element;
-
-  it('should make hidden element visible', inject(function ($rootScope, $compile) {
-    element = angular.element('<dragdrop></dragdrop>');
-    element = $compile(element)($rootScope);
-    expect(element.text()).toBe('this is the dragdrop directive');
-  }));
+//
+//  var element;
+//
+//  it('should make hidden element visible', inject(function ($rootScope, $compile) {
+//    element = angular.element('<dragdrop></dragdrop>');
+//    element = $compile(element)($rootScope);
+//    expect(element.text()).toBe('this is the dragdrop directive');
+//  }));
 });
