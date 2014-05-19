77393e86939e92b80d771eaee04f49b5e03b215a
itsikm <itsik@gigspaces.com>
2014-05-19 11:51:46 +0300

    Multi Bar Chart

---
+++ test/spec/controllers/multiBarChart.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: MultibarchartCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('gsUiKsApp'));
+
+  var MultibarchartCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    MultibarchartCtrl = $controller('MultibarchartCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
+});
